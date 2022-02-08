2cceedb2da81d21f9bdcfedc111d3d99e9f7ed5f
Vi Vo <aaronvo118@gmail.com>
2022-02-07 20:13:18 -0600

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", (events) => {
+    events.preventDefault()
+    handleToDo(events.target.newTaskDescription.value)
+
+   form.reset()
+  })
 });
+
+function handleToDo(todo) {
+  //create a p element
+  let p = document.createElement("p")
+  p.textContent = `${todo} `
+
+  
+  // create the 'done' button and add function
+  let bttn = document.createElement("button")
+
+  bttn.addEventListener("click", () => {
+    p.remove()
+  })
+
+  bttn.textContent = "Done"
+
+
+
+
+//add to todo list 
+  p.appendChild(bttn)
+  document.querySelector("#list").appendChild(p)
+
+  console.log(p)
+
+}
+
